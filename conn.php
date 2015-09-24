b1007e7f7d29b8601216952af201175581a4e8d2
hubbard <13420909607@163.com>
2015-09-24 11:58:24 +0800

    修改数据库配置

--- conn.php
+++ conn.php
@@ -1,9 +1,9 @@
 <?php
-$conn = @ mysql_connect("localhost", "root", "","test");
-mysql_select_db("app_hubbard", $conn);
+
+$conn = mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);if(!$conn){ die('could
+ not connect:'.mysql_error()); }mysql_select_db(SAE_MYSQL_DB,$conn);
 mysql_query("set names 'utf8'");
 if(mysqli_connect_errno()) {
     echo 'Error:dcgggggggg';
     exit;
-}
-?>
+}?>
